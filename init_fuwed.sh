302bff4260bef7b869d2ec76cb431eaa6ac287e1
Kreijstal <rainb@tfwno.gf>
2020-05-25 16:05:44 +0200

    Update init_fuwed.sh

--- init_fuwed.sh
+++ init_fuwed.sh
@@ -1,2 +1,3 @@
 wget https://upload.wikimedia.org/wikipedia/commons/6/63/Twemoji2_1f914.svg
 wget https://upload.wikimedia.org/wikipedia/commons/8/8f/Icon_sound_radio.svg
+wget https://upload.wikimedia.org/wikipedia/commons/d/da/Electromagnetic_wave.svg
