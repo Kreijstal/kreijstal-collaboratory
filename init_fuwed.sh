2643d3f49e28b13c5457f2e9397d56f1edb00f75
Kreijstal <rainb@tfwno.gf>
2020-05-27 09:25:10 +0200

    Update init_fuwed.sh

--- init_fuwed.sh
+++ init_fuwed.sh
@@ -3,3 +3,5 @@ wget https://upload.wikimedia.org/wikipedia/commons/8/8f/Icon_sound_radio.svg
 wget https://upload.wikimedia.org/wikipedia/commons/d/da/Electromagnetic_wave.svg
 wget https://cdn.discordapp.com/attachments/709470048196231249/714576619880316979/bardeen-3.jpg
 wget https://upload.wikimedia.org/wikipedia/commons/5/59/Julius_Edgar_Lilienfeld_%281881-1963%29.jpg
+wget https://upload.wikimedia.org/wikipedia/commons/9/9b/BJT_PNP_symbol.svg
+wget https://upload.wikimedia.org/wikipedia/commons/6/66/BJT_NPN_symbol.svg
